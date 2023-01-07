806ad472253bbf35422cecebfdc582cdeadb7ea0
rioto.nagano <kingdom950927@gmail.com>
2023-01-08 05:59:42 +0900

    useReducerを用いたカウントボタンの作成

--- 11_hooks_p1/src/025_practice_useReducer/start/Example.js
+++ 11_hooks_p1/src/025_practice_useReducer/start/Example.js
@@ -2,7 +2,28 @@ import { useReducer } from "react";
 
 const CALC_OPTIONS = ["add", "minus", "divide", "multiply"];
 
-const reducer = () => {}
+const reducer = (state, {type, payload}) => {
+  switch (type) {
+    case "change": {
+      const { name, value } = payload;
+      return { ...state, [name]: value };
+    }
+    case "add": {
+      return { ...state, result: state.a + state.b };
+    }
+    case "minus": {
+      return { ...state, result: state.a - state.b };
+    }
+    case "divide": {
+      return { ...state, result: state.a / state.b };
+    }
+    case "multiply": {
+      return { ...state, result: state.a * state.b };
+    }
+    default:
+      throw new Error("operator is invalid");
+  }
+}
 
 const Example = () => {
   const initState = {
@@ -14,11 +35,11 @@ const Example = () => {
   const [state, dispatch] = useReducer(reducer, initState);
 
   const calculate = (e) => {
-
+    dispatch({type: e.target.value});
   };
 
   const numChangeHandler = (e) => {
-
+    dispatch({type: "change", payload: { name:  e.target.name, value: e.target.value}})
   }
 
   return (

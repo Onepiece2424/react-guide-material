05c760d0eeb5a038094006b1c7309e76e2c3ad34
rioto.nagano <kingdom950927@gmail.com>
2023-01-07 13:23:47 +0900

    npm install

--- 11_hooks_p1/src/010_useState_to_useReducer/start/Example.js
+++ 11_hooks_p1/src/010_useState_to_useReducer/start/Example.js
@@ -1,7 +1,40 @@
+import { useState,useReducer } from 'react'
+
 const Example = () => {
-  
+
+  const [ state, setState ] = useState(0)
+  const [ rstate, dispatch ] = useReducer((prev, action) => {
+    if (action === '+') {
+      return ++prev
+    } else if (action === '-') {
+      return --prev
+    }
+  }, 0)
+
+
+  const countUp = () => {
+    setState(prev => ++prev)
+  }
+
+  const rcountUp = () => {
+    dispatch('+')
+  }
+
+  const rcountDown = () => {
+    dispatch('-')
+  }
+
   return (
     <>
+      <div>
+        <h3>{state}</h3>
+        <button onClick={countUp}>+</button>
+      </div>
+      <div>
+        <h3>{rstate}</h3>
+        <button onClick={rcountUp}>+</button>
+        <button onClick={rcountDown}>-</button>
+      </div>
     </>
   );
 };

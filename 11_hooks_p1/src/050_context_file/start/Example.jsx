aaef3a8e26a4eb696399db26d1984c807e823d2a
rioto.nagano <kingdom950927@gmail.com>
2023-01-08 13:55:37 +0900

    ラジオボタン変更時にスタイルが変更する機能の作成

--- 11_hooks_p1/src/050_context_file/start/Example.jsx
+++ 11_hooks_p1/src/050_context_file/start/Example.jsx
@@ -3,15 +3,22 @@ import "./Example.css";
 
 const Example = () => {
   const [theme, setTheme] = useState('light')
-  
+
   const changeTheme = (e) => setTheme(e.target.value)
-  
+
   const THEMES = ['light', 'dark', 'red'];
 
   return (
     <>
       <header className={`content-${theme}`}>
-        
+        {THEMES.map(_theme => {
+          return (
+            <label>
+              <input type='radio' key={_theme} value={_theme} checked={theme === _theme} onChange={changeTheme} />
+              {_theme}
+            </label>
+          )
+        })}
       </header>
       <main className={`content-${theme}`}>
         <h1>テーマの切り替え</h1>

e59c193f3e4c8d666dfd020f695a411b39e63c27
rioto.nagano <kingdom950927@gmail.com>
2023-01-08 14:16:27 +0900

    ExampleコンポーネントにあったHeader,Main部分を各コンポーネントへ移行

--- 11_hooks_p1/src/050_context_file/start/Example.jsx
+++ 11_hooks_p1/src/050_context_file/start/Example.jsx
@@ -1,29 +1,18 @@
-import { useState } from "react";
+import { createContext,useState } from "react";
 import "./Example.css";
+import Main from "./components/Main";
+import Header from "./components/Header";
+
+export const ThemeContext = createContext()
 
 const Example = () => {
   const [theme, setTheme] = useState('light')
 
-  const changeTheme = (e) => setTheme(e.target.value)
-
-  const THEMES = ['light', 'dark', 'red'];
-
   return (
-    <>
-      <header className={`content-${theme}`}>
-        {THEMES.map(_theme => {
-          return (
-            <label>
-              <input type='radio' key={_theme} value={_theme} checked={theme === _theme} onChange={changeTheme} />
-              {_theme}
-            </label>
-          )
-        })}
-      </header>
-      <main className={`content-${theme}`}>
-        <h1>テーマの切り替え</h1>
-      </main>
-    </>
+    <ThemeContext.Provider value={[theme, setTheme]}>
+      <Header />
+      <Main />
+    </ThemeContext.Provider>
   );
 };
 

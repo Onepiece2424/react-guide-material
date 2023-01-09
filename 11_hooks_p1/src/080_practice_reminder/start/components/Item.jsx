2361298d87fe26b2e613d77129f0400c51b528b6
rioto.nagano <kingdom950927@gmail.com>
2023-01-09 11:56:33 +0900

    useContextとuseReducerを使用したボタンの作成

--- 11_hooks_p1/src/080_practice_reminder/start/components/Item.jsx
+++ 11_hooks_p1/src/080_practice_reminder/start/components/Item.jsx
@@ -1,26 +1,33 @@
 import { useState } from "react"
+import { useDispatchTodos } from "../context/TodoContext"
 
-const Item = ({ todo, complete, updateTodo  }) => {
+const Item = ({ todo }) => {
 
   const [ editingContent, setEditingContent ] = useState(todo.content)
+
+  const dispatch = useDispatchTodos();
 
   const changeContent = (e) => setEditingContent(e.target.value)
 
   // 2回クリックされると実行
   const toggleEditMode = () => {
     const newTodo = { ...todo, editing: !todo.editing }
-    updateTodo(newTodo)
+    dispatch({type: 'todo/update', todo: newTodo})
   }
 
   const confirmContent = (e) => {
     e.preventDefault();
     const newTodo = { ...todo, editing: !todo.editing, content: editingContent }
-    updateTodo(newTodo)
+    dispatch({type: 'todo/update', todo: newTodo})
+  }
+
+  const complete = (todo) => {
+    dispatch({ type: 'todo/delete', todo } )
   }
 
   return (
     <div key={todo.id}>
-      <button onClick={() => complete(todo.id)}>完了</button>
+      <button onClick={() => complete(todo)}>完了</button>
       <form onSubmit={confirmContent} style={{ display: 'inline' }}>
         {
         todo.editing ? (

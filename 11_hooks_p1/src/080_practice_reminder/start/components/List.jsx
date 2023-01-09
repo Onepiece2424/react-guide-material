0897ee7049e95e7a4f492ea21533d592501cd456
rioto.nagano <kingdom950927@gmail.com>
2023-01-09 09:59:37 +0900

    Item　componentの作成

---
+++ 11_hooks_p1/src/080_practice_reminder/start/components/List.jsx
@@ -0,0 +1,14 @@
+import Item from "./Item";
+
+const List = ({todos, deleteTodo}) => {
+    const complete = (id) => {
+        deleteTodo(id)
+    }
+    return (
+        <div>
+            {todos.map(todo => <Item todo={todo} complete={complete} key={todo.id}/>)}
+        </div>
+    );
+}
+
+export default List;

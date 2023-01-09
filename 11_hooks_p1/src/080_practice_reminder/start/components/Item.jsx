000cdc6f12167262013d12398c07ebe580a45de4
rioto.nagano <kingdom950927@gmail.com>
2023-01-09 10:49:43 +0900

    ダブルクリックした時フォームに変換

--- 11_hooks_p1/src/080_practice_reminder/start/components/Item.jsx
+++ 11_hooks_p1/src/080_practice_reminder/start/components/Item.jsx
@@ -1,8 +1,27 @@
-const Item = ({ todo, complete }) => {
+import { useState } from "react"
+
+const Item = ({ todo, complete, updateTodo  }) => {
+
+  const [ editingContent, setEditingContent ] = useState(todo.content)
+
+  const changeContent = (e) => setEditingContent(e.target.value)
+
+  // 2回クリックされると実行
+  const toggleEditMode = () => {
+    const newTodo = { ...todo, editing: !todo.editing }
+    updateTodo(newTodo)
+  }
+
   return (
     <div key={todo.id}>
       <button onClick={() => complete(todo.id)}>完了</button>
-      <span>{todo.content}</span>
+      {
+        todo.editing ? (
+          <input type="text" value={editingContent} onChange={changeContent} />
+        ) : (
+          <span onDoubleClick={toggleEditMode}>{todo.content}</span>
+        )
+      }
     </div>
   )
 }
--- 11_hooks_p1/src/080_practice_reminder/start/components/List.jsx
+++ 11_hooks_p1/src/080_practice_reminder/start/components/List.jsx
@@ -1,12 +1,12 @@
 import Item from "./Item";
 
-const List = ({todos, deleteTodo}) => {
+const List = ({todos, deleteTodo, updateTodo}) => {
     const complete = (id) => {
         deleteTodo(id)
     }
     return (
         <div>
-            {todos.map(todo => <Item todo={todo} complete={complete} key={todo.id}/>)}
+            {todos.map(todo => <Item todo={todo} complete={complete} key={todo.id} updateTodo={updateTodo} />)}
         </div>
     );
 }
